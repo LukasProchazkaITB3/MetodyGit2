049fb9c404f5c224633addb60ce3028997cb5004
Lycky_LITH <prochazkal.03@spst.eu>
2021-03-21 15:09:07 +0100

    Button1 - Za 100 dní

---
+++ MetodyGit2/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetodyGit2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime dnes = DateTime.Today;
+            DateTime za100dni = dnes.AddDays(100);
+
+            label1.Text = za100dni.ToLongDateString();
+
+        }
+    }
+}
